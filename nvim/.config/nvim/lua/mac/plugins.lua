5d8dc93d3d14a845205198540be423468b613445
ananta <12180395+ananta@users.noreply.github.com>
2022-08-27 21:43:14 -0400

    feat(vimwiki): add VimWiki to workflow

--- nvim/.config/nvim/lua/mac/plugins.lua
+++ nvim/.config/nvim/lua/mac/plugins.lua
@@ -125,6 +125,9 @@ return packer.startup({
 	-- Discord
 	use("andweeb/presence.nvim")
 
+  -- Notes using VimWiki
+  use("vimwiki/vimwiki")
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
